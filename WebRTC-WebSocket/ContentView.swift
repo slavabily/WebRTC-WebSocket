a15730351d1745eef6b90dec74d0cb41dfa84ede
slava bily <slavabily@me.com>
2021-06-03 17:37:41 +0300

    first commit

---
+++ WebRTC-WebSocket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebRTC-WebSocket
+//
+//  Created by slava bily on 03.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
